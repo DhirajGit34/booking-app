70df87806df2901d5163572912d7e626645780fa
Dhiraj Deka <96738706+DhirajGit34@users.noreply.github.com>
2025-09-14 00:21:41 +0530

    remove data file

--- src/data/sql/add_extras_price_column.sql
+++
@@ -1,8 +0,0 @@
--- Add extrasPrice column to bookings table
-ALTER TABLE bookings
-ADD COLUMN "extrasPrice" NUMERIC(10,2) DEFAULT 0;
-
--- Update existing rows to have a default value
-UPDATE bookings
-SET "extrasPrice" = 0
-WHERE "extrasPrice" IS NULL;
